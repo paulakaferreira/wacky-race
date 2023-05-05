5322de31703125c507f463c574d0137281740aff
Paula <paulakaferreira@gmail.com>
2023-05-05 10:35:26 +0200

    create package, add main class

---
+++ wackyrace/WakyRace.java
@@ -0,0 +1,14 @@
+package wackyrace;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class WakyRace {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter the team's name and number
+        System.out.println("Enter the team's name:");
+        String teamName = scanner.nextLine();
+    }
+}
